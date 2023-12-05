af5fe9a164db9e79a9f73a8a94485d890c4e3c53
Rositsa <i52443@fhict.nl>
2023-12-05 01:28:46 +0100

    Add project files.

---
+++ Project Solution for Student Houses/Form1.cs
@@ -0,0 +1,10 @@
+namespace Project_Solution_for_Student_Houses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
