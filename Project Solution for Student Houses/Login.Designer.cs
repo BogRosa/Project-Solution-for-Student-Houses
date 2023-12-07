dbb182d7b879e3d696a635f653636efd8eb58ed5
Dobrev,Konstantin K.N <k.dobrev@student.fontys.nl>
2023-12-07 11:01:34 +0100

    changed the name of the login page

--- Project Solution for Student Houses/Login.Designer.cs
+++ Project Solution for Student Houses/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Project_Solution_for_Student_Houses
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
--- Project Solution for Student Houses/Login.cs
+++ Project Solution for Student Houses/Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Project_Solution_for_Student_Houses
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
